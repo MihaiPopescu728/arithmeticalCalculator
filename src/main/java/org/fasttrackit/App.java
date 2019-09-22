79e9d3a6a6f819de7e8d2ecf05b2daedad5316a0
Mihai728 <mihaipopescu728@yahoo.com>
2019-09-22 21:12:40 +0300

    Folosirea metodelor cu diferite tipuri de valori returnate Supra scrierea metodelor

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,10 @@
+package org.fasttrackit;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(OperationsCalculator.computeSum(10, 20));
+        System.out.println(OperationsCalculator.computeSum(20, 40,50));
+        System.out.println(OperationsCalculator.computeSum(5.0, 4.0));
+        System.out.println(OperationsCalculator.computeSum(23));
+    }
+}
---
+++ src/main/java/org/fasttrackit/OperationsCalculator.java
@@ -0,0 +1,20 @@
+package org.fasttrackit;
+
+public class OperationsCalculator {
+
+    public static double computeSum( int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public static double computeSum (double number1, double number2, double number3) {
+        return number1 * number2 * number3;
+    }
+
+    public static double computeSum(double number1, double number2) {
+        return number1 - number2;
+    }
+    public static double computeSum (double number1) {
+        return number1 / 10;
+    }
+
+}
